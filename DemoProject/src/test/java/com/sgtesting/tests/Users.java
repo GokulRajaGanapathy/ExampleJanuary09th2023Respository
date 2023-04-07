1ec13964a4b79888ebca95368d10f978cb6f8f0e
GokulRajaGanapathy <gokulrajas2016@gmail.com>
2023-04-07 08:35:30 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,31 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("It is a method 1");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("It is a method 2");
+	}
+	
+	@Test
+	public void method3()
+	{
+		System.out.println("It is a method 3");
+	}
+	
+	@Test
+	public void method4()
+	{
+		System.out.println("It is a method 4");
+	}
+}
